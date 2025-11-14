11a6de0b1be477ece7e26f55ae1d7a2519c1a0eb
himitu-da <0127himitu@gmail.com>
2025-11-14 16:33:29 +0900

    feat: tsconfig.jsonとwebpack.config.jsを追加

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: './src/main.ts',
+    renderer: './src/renderer.tsx'
+  },
+  target: 'electron-renderer',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
